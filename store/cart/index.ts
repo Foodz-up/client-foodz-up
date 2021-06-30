b0f1a67c9915f6a317dd3c8bf4265c6b64623519
AntoineMONDOT <antoine.mondot@viacesi.fr>
2021-06-30 10:04:39 +0200

    ✨ Add Notifications

--- store/cart/index.ts
+++ store/cart/index.ts
@@ -2,6 +2,7 @@ import BaseStoreService from '~/store/abstract'
 import { CartStoreModule } from '~/store/cart/module'
 import { CartState } from '~/store/cart/state'
 import { ICart, IMenu, IArticle } from '~/store/interfaces/'
+import NotificationStore from '~/store/notification'
 
 class CartStore extends BaseStoreService<CartState> {
   public mutations = CartStoreModule.mutations
@@ -13,14 +14,26 @@ class CartStore extends BaseStoreService<CartState> {
 
   setCart (items: Array<ICart>) {
     this.commit(this.mutations.setCart, items)
+    NotificationStore.addNotification({
+      message: 'Votre panier à bien été vidé',
+      status: 200
+    })
   }
 
   addItemsToCart (quantity: number, item: IMenu | IArticle, restaurantId: number) {
     this.commit(this.mutations.addItemsToCart, { quantity, item, restaurantId })
+    NotificationStore.addNotification({
+      message: 'Votre articles à bien été ajouté',
+      status: 200
+    })
   }
 
   removeItemsFromCart (idItem: number) {
     this.commit(this.mutations.removeItemsFromCart, idItem)
+    NotificationStore.addNotification({
+      message: 'Votre articles à bien été supprimé',
+      status: 200
+    })
   }
 }
 export default new CartStore()
